8e90f159420918b61d94983177c57e5c559915cd
Jairo <jairo.alcantar@hotmail.com>
2020-08-18 18:52:54 -0500

    Initial commit

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import {Router} from '@angular/router';
+
+import {
+  AngularFirestore,
+  AngularFirestoreDocument
+} from '@angular/fire/firestore';
+
+
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+
+  constructor(
+  ) {
+
+  }
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyA5-0X8sjan3TPt6t2aQZ8BvHtlltEz3RQ",
+    authDomain: "firstproject-87ae2.firebaseapp.com",
+    databaseURL: "https://firstproject-87ae2.firebaseio.com",
+    projectId: "firstproject-87ae2",
+    storageBucket: "firstproject-87ae2.appspot.com",
+    messagingSenderId: "860604238316",
+    appId: "1:860604238316:web:b68f5875c78b0faeb0d8c9",
+    measurementId: "G-W370KR4ZG9"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
